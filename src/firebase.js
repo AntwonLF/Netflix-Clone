f003eeb523079a6eeb51c8374a4aa33ae6ad0fe9
Antwon Frager Jr <Antwon.frager@gmail.com>
2024-03-07 11:11:11 -0500

    Started Netflix clone project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDK0w5nWdL3rItDybWCqHnFGs2IN-g8Eyg",
+    authDomain: "netflix-clone-e6394.firebaseapp.com",
+    projectId: "netflix-clone-e6394",
+    storageBucket: "netflix-clone-e6394.appspot.com",
+    messagingSenderId: "1283863211",
+    appId: "1:1283863211:web:e23ec90ddea6ec04cc3dee"
+  };
