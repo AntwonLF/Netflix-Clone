f3515adf98d3844d260cf1f50da293f202887f57
Antwon Frager Jr <Antwon.frager@gmail.com>
2024-03-07 11:37:58 -0500

    worked on HomeScreen and Nav

---
+++ src/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './Nav'
+
+function HomeScreen() {
+  return (
+    <div className='homeScreen'>
+        <Nav />
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ src/Nav.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    <div className='nav'>
+      <div className='nav_contents'>
+        <img
+          className='nav_logo'
+          src="https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940"
+          alt=""
+        />
+
+        <img
+          className='nav_avatar'
+          src="https://i.pinimg.com/474x/10/12/c0/1012c06c7e1b0f8f5e60611992785e5a.jpg"
+          alt=""
+        />
+
+      </div>
+    </div >
+  )
+}
+
+export default Nav
